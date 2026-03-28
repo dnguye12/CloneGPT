9c1c57e7290427f1f277b4b7ea1bb4079b0923e9
Duc Huy Nguyen <huy.nguyen@coderpad.io>
2026-03-28 21:11:28 +0100

    hydration bug with ThemeToggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -3,9 +3,26 @@
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
     const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState<boolean>(false)
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return <Button
+            variant="ghost"
+            size="icon-lg"
+        >
+            <SunIcon />
+        </Button>
+    }
+
     return (
         <Button
             variant="ghost"
