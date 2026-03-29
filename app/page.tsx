b476242f0cbeb254605243dc2d4ec61420c29819
Duc Huy Nguyen <huy.nguyen@coderpad.io>
2026-03-29 16:35:46 +0200

    better code structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import HomeInput from "./components/HomeInput";
 
 
 export default function Home() {
+
     return (
-        <div className="container mx-auto max-w-3xl flex justify-center items-center h-[calc(100vh-64px)] px-6">
+        <div className="main-container">
             <div className="w-full">
                 <h1 className=" text-center whitespace-pre-wrap text-pretty text-3xl">What are you working on?</h1>
                 <HomeInput />
